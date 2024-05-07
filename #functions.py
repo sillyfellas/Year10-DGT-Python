43afc8befc8f2b29b4d1aa23cac81e2b7ead5923
sillyfellas <168596655+sillyfellas@users.noreply.github.com>
2024-05-08 10:01:53 +1200

    Add files via upload

---
+++ #functions.py
@@ -0,0 +1,9 @@
+#functions go here
+def intro():
+    print("Welcome")
+    print()
+    print("Instructions go here")
+    retrun "" #I am returning all (displaying/using) all items.
+
+# Main routine
+intro() # This calls the function so that we can use it
