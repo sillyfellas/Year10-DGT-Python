f18b78f93f49879a1b1c920aef81c17018ecdf8d
sillyfellas <168596655+sillyfellas@users.noreply.github.com>
2024-05-10 14:34:30 +1200

    Delete #functions.py

--- #functions.py
+++
@@ -1,9 +0,0 @@
-#functions go here
-def intro():
-    print("Welcome")
-    print()
-    print("Instructions go here")
-    retrun "" #I am returning all (displaying/using) all items.
-
-# Main routine
-intro() # This calls the function so that we can use it
