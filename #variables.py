e53dfcbb3e9e490d47a8360db63ce081bde64cb9
sillyfellas <168596655+sillyfellas@users.noreply.github.com>
2024-05-10 14:28:01 +1200

    Update #variables.py

--- #variables.py
+++ #variables.py
@@ -6,6 +6,7 @@ greeting = "hello world"
 # Variables can be a number
 fav_num = 7
 
+
 # Variables can be Boonlen (True/False)
 love_programming = True
 
@@ -15,3 +16,25 @@ print(love_programming)
 
 # This variable has a problem
 # my name = "Ecko"
+
+
+
+
+#variables
+
+# Variables can be a string 
+greeting = "hello world"
+
+# Variables can be a number
+fav_num = 7
+num_string = "5"
+
+# Variables can be Boonlen (True/False)
+love_programming = True
+
+print(fav_num * 2)
+print(num_string * 5)
+
+# watch out for quotation marks. Python will treat it as text!!!
+
+# constants are an uppercase
