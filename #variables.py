dfa380dc6e2ab1f32628cbe368e3e7e7014c4fbe
sillyfellas <168596655+sillyfellas@users.noreply.github.com>
2024-05-10 14:19:24 +1200

    Add files via upload

---
+++ #variables.py
@@ -0,0 +1,17 @@
+#variables
+
+# Variables can be a string 
+greeting = "hello world"
+
+# Variables can be a number
+fav_num = 7
+
+# Variables can be Boonlen (True/False)
+love_programming = True
+
+print(greeting)
+print(fav_num)
+print(love_programming)
+
+# This variable has a problem
+# my name = "Ecko"
