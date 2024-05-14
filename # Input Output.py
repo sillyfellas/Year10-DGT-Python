5efc594b54004e154419e3e096f07ab0c5ca6da3
sillyfellas <168596655+sillyfellas@users.noreply.github.com>
2024-05-15 09:33:03 +1200

    Add files via upload

---
+++ # Input Output.py
@@ -0,0 +1,23 @@
+# Input Output
+
+# ask the user for their name
+username = input ("What is your name good sir?")
+
+# ask user for their favorite interger (number)
+fav_num = int(input("Cool name, What is your favourite number?"))
+
+# double, halve and square the user's favorite number
+double = fav_num * 2
+halve = fav_num / 2
+square = fav_num * fav_num
+
+# greet the user
+print(f"\nHi {username}, your favourite number is {fav_num}")
+
+# Output results of doubling, halving and squaring their favorite number (interger)
+# squaring their favorite interger (number)
+print(f"Double {fav_num} is {double}")
+print(f"Half {fav_num} is {halve}")
+print(f"{fav_num} squared is {square}")
+print()
+print(f"HAVE A NICE DAY!")    
